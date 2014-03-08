62e188382542c3a8dd1e277ee4d5e009661d90d5
Joakim Bengtson <joakim.bengtson@gmail.com>
2014-03-08 16:03:54 +0100

    First version

---
+++ index.js
@@ -0,0 +1,135 @@
+'use strict';
+var through2 = require('through2'),
+    inquirer = require('inquirer'),
+    gutil = require('gulp-util'),
+    diff = require('diff'),
+    fs = require('fs'),
+    path = require('path'),
+    pkg = require('./package');
+
+module.exports = function conflict (dest, opt) {
+  if (!dest) {
+    error('Missing destination dir parameter!');
+  }
+
+  opt = opt || {};
+
+  var all = false;
+
+  return through2.obj(function (file, enc, cb) {
+    var newPath = path.resolve(opt.cwd || process.cwd(), dest, file.relative);
+    fs.exists(newPath, function (exists) {
+      if (!all && exists) {
+        fs.readFile(newPath, 'utf8', function (err, contents) {
+          if (err) {
+            error('Reading old file for comparison failed with: ' + err.message);
+          }
+          if (contents === String(file.contents)) {
+            log('Skipping ' + file.relative + ' (identical)');
+            return cb();
+          }
+          var askCb = function askCb (action) {
+            switch (action) {
+              case 'all':
+                all = true;
+                /* falls through */
+              case 'replace':
+                log('Keeping ' + file.relative);
+                this.push(file);
+                break;
+              case 'skip':
+                log('Skipping ' + file.relative);
+                break;
+              case 'end':
+                log(gutil.colors.red('Aborting...'));
+                process.exit(0);
+                break;
+              case 'diff':
+                log('Showing diff for ' + file.relative);
+                diffFiles(file, newPath);
+                ask(file, askCb.bind(this));
+                return;
+            }
+            cb();
+          };
+          ask(file, askCb.bind(this));
+        }.bind(this));
+      } else {
+        log('Keeping ' + file.relative);
+        this.push(file);
+        cb();
+      }
+    }.bind(this));
+  });
+};
+
+function ask (file, cb) {
+  inquirer.prompt([{
+    type: 'expand',
+    name: 'replace',
+    message: 'Replace ' + file.relative + '?',
+    choices: [{
+      key: 'y',
+      name: 'replace',
+      value: 'replace'
+    }, {
+      key: 'n',
+      name: 'do not replace',
+      value: 'skip'
+    }, {
+      key: 'a',
+      name: 'replace this and all others',
+      value: 'all'
+    }, {
+      key: 'x',
+      name: 'abort',
+      value: 'end'
+    }, {
+      key: 'd',
+      name: 'show the differences between the old and the new',
+      value: 'diff'
+    }]
+  }],
+  function (answers) {
+    cb(answers.replace);
+  });
+}
+
+function diffFiles (newFile, oldFilePath) {
+  if (newFile.isStream()) {
+    error('Diff does not support file streams');
+  }
+  try {
+    var content = fs.readFileSync(oldFilePath, 'utf8');
+    var differences = diff.diffLines(content, String(newFile.contents));
+    log('File differences: ' + gutil.colors.bgGreen('added') + ' ' + gutil.colors.bgRed('removed') + '\n\n' + differences.map(formatPart).join(''));
+  } catch (err) {
+    error('Reading old file for diff failed with: ' + err.message);
+  }
+}
+
+function formatPart (part, i) {
+  var indent = new Array(8).join(' ');
+  return (!i ? indent : '') + part.value.split('\n').map(function (line) {
+    return gutil.colors[colorFromPart(part)](line);
+  }).join('\n' + indent);
+}
+
+function colorFromPart (part) {
+  if (part.added) {
+    return 'bgGreen';
+  } else if (part.removed) {
+    return 'bgRed';
+  }
+  return 'grey';
+}
+
+function log () {
+  var logger = gutil.log.bind(gutil, '[' + gutil.colors.cyan('conflict') + ']');
+  logger.apply(logger, arguments);
+}
+
+function error (message) {
+  throw new gutil.PluginError(pkg.name, message);
+}
+
---
+++ test/conflict_test.js
@@ -0,0 +1,219 @@
+/* jshint camelcase: false, strict: false */
+/* global describe, beforeEach, afterEach, it */
+var chai = require('chai'),
+    should = chai.should(),
+    inquirer = require('inquirer'),
+    diff = require('diff'),
+    gutil = require('gulp-util'),
+    path = require('path'),
+    conflict = require('../.');
+
+function fixture (file) {
+  var filepath = path.join(__dirname, file);
+  return new gutil.File({
+    path: filepath,
+    cwd: __dirname,
+    base: path.join(__dirname, path.dirname(file)),
+    contents: null
+  });
+}
+
+describe('gulp-conflict', function () {
+  var exitCalled = false,
+      diffCalled = false,
+      origExit = null,
+      origDiff = null;
+
+  beforeEach(function () {
+    origExit = process.exit;
+    process.exit = function () {
+      exitCalled = true;
+    };
+    origDiff = diff.diffLines;
+    diff.diffLines = function () {
+      diffCalled = true;
+      return [];
+    };
+  });
+
+  afterEach(function () {
+    process.exit = origExit;
+    exitCalled = false;
+    diff.diffLines = origDiff;
+    diffCalled = false;
+  });
+
+  it('should keep conflicting file on `y) replace`', function (done) {
+    var file = fixture(__filename);
+
+    mockPrompt({
+      replace: 'replace'
+    });
+
+    var stream = conflict(__dirname);
+
+    stream.on('error', function(err) {
+      should.exist(err);
+      done(err);
+    });
+
+    stream.on('data', function (file) {
+      should.exist(file);
+      file.relative.should.equal(path.basename(__filename));
+      done();
+    });
+
+    stream.write(file);
+
+    stream.end();
+  });
+
+  it('should skip conflicting file on `n) do not replace`', function (done) {
+    var file = fixture(__filename);
+
+    mockPrompt({
+      replace: 'skip'
+    });
+
+    var stream = conflict(__dirname);
+
+    stream.on('error', function(err) {
+      should.exist(err);
+      done(err);
+    });
+
+    stream.on('data', function (file) {
+      should.not.exist(file);
+      done();
+    });
+
+    stream.on('end', function () {
+      done();
+    });
+
+    stream.write(file);
+
+    stream.end();
+  });
+
+  it('should keep all conflicting files on `a) replace this and all...`', function (done) {
+    var files = [
+      fixture(__filename),
+      fixture('test.json')
+    ];
+
+    mockPrompt({
+      replace: 'all'
+    });
+
+    var stream = conflict(__dirname),
+        count = 0;
+
+    stream.on('error', function(err) {
+      should.exist(err);
+      done(err);
+    });
+
+    stream.on('data', function (file) {
+      should.exist(file);
+      count += 1;
+    });
+
+    stream.on('end', function () {
+      count.should.equal(2);
+      done();
+    });
+
+    files.forEach(function (file) {
+      stream.write(file);
+    });
+
+    stream.end();
+  });
+
+  it('should abort (exit process) on `x) abort`', function (done) {
+    var file = fixture(__filename);
+
+    mockPrompt({
+      replace: 'end'
+    });
+
+    var stream = conflict(__dirname);
+
+    stream.on('error', function(err) {
+      should.exist(err);
+      done(err);
+    });
+
+    stream.on('data', function (file) {
+      should.not.exist(file);
+      done();
+    });
+
+    stream.on('end', function () {
+      exitCalled.should.equal(true);
+      done();
+    });
+
+    exitCalled.should.equal(false);
+
+    stream.write(file);
+
+    stream.end();
+  });
+
+  it('should show diff between old and new files on `d) show diff...`', function (done) {
+    var file = fixture('test.json');
+
+    mockPrompt([{replace: 'diff'}, {replace: 'replace'}]);
+
+    var stream = conflict(__dirname);
+
+    stream.on('error', function(err) {
+      should.exist(err);
+      done(err);
+    });
+
+    stream.on('data', function (file) {
+      should.exist(file);
+      file.relative.should.equal('test.json');
+      diffCalled.should.equal(true);
+      done();
+    });
+
+    diffCalled.should.equal(false);
+
+    stream.write(file);
+
+    stream.end();
+  });
+});
+
+/**
+ * Mock inquirer prompt (snippet from [Yeoman generator helpers](https://github.com/yeoman/generator/blob/master/lib/test/helpers.js))
+ *
+ * Enhanced to support multiple different answers to the same question (useful if called recursively to avoid infinite loop)
+ */
+function mockPrompt (answers) {
+  var origPrompt = inquirer.prompt;
+  inquirer.prompt = function (prompts, done) {
+    var answ;
+    if (Array.isArray(answers)) {
+      answ = answers.shift();
+    } else {
+      answ = answers;
+    }
+
+    if (!Array.isArray(prompts)) {
+      prompts = [prompts];
+    }
+
+    prompts.forEach(function (prompt) {
+      if (!(prompt.name in answ)) {
+        answ[prompt.name] = prompt.default;
+      }
+    });
+    done(answ);
+  };
+  inquirer.origPrompt = origPrompt;
+}
